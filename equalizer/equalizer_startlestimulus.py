197132ee582c9b5654f279c59c2a9911bb25cbbe
hinrah <hinrah@localhost>
2018-02-16 13:21:19 +0100

    Changed equalizer file to make using easier there are now two files, one for prestim and one for starle stim

--- equalizer/equalizer_startlestimulus.py
+++ equalizer/equalizer_startlestimulus.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 12 12:40:04 2018
-
-@author: rahlfshh
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Mon Nov 27 13:16:07 2017
 
 @author: rahlfshh
@@ -15,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
 import time
+np.random.seed(1337)
 
 plt.rc("font", size=20)
 plt.rc("xtick", labelsize=20)
@@ -32,7 +26,7 @@ plt.rc("lines", markersize=10)
 
 plt.rc("figure.subplot",left=0.15)  
 plt.rc("figure.subplot",right=0.95)    
-plt.rc("figure.subplot",bottom=0.15)     
+plt.rc("figure.subplot",bottom=0.05)     
 plt.rc("figure.subplot",top=0.95)
 
 
@@ -42,66 +36,77 @@ def simpleaxis(ax):
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
 
-# Parameters
-# general parameters
-#TODO das könnte man auch mit 96000 machen, aber das dauert sehr lange, brauchen wir das?
 fs = 96000 # sampling rate in Hz
-duration = 3 # signal duration in seconds
+duration = 2 #signal duration in seconds
+adjust_duration = 1#signal duration for adjustment
 
 # LMS algorithm
-mu = 0.04# stepsize for LMS algorithm
-L = 2048# filter length for LMS algorithm in samples
+mu = 0.2# stepsize for LMS algorithm
+L = 4096# filter length for LMS algorithm in samples
 
 # equalizing filter computation
-L_eq = 2048
-delta = 1e-2
+L_eq = 4096
+delta = 0.001
 nFFT = 10*L_eq# heuristical choice; must be >= than L_eq
 
+#start noisefactor will, be increased apropriately
+noisefactor_min = 0.00
+#maximum noisefactor. Be careful, if the amplifier is to strong this needs to be lowered as your speeker might be destroyed otherwise
+noisefactor_max = 0.003
 
-## Artificial impulse response
-# normally, this impulse response is not known and instead estimated, e.g.,
-# via the (N)LMS algorithm (see below)
-#h = double((1:L).'==20) + double((1:L).' > 20) .* exp(-((1:L).'-20)*0.01) .* randn(L,1);
-#h = np.linspace(1,0,num = L)
-#h = fftfilt(ones(5,1)./5,h); % averaging with sliding windows acts as a lowpass filter (=> higher frequencies attenuated)
+noisefactor = (noisefactor_min + noisefactor_max)/2
+
+max_rec = 0
+
+adjust_x = np.random.randn(fs*adjust_duration)
+
+#find the right adjustment factor
+while max_rec < 0.2 or max_rec >0.25:
+    to_play = np.dstack((np.zeros(len(adjust_x)),adjust_x))[0]
+    d_adjust = sd.playrec(to_play*noisefactor, samplerate = fs, channels = 1,device = [1,7])
+    time.sleep(adjust_duration+2)
+    ################d_adjust = np.convolve(h,adjust_x)*noisefactor
+    ################d_adjust += np.random.randn(len(d_adjust))*0.01
+    
+    
+    max_rec = max(d_adjust)
+    print("max_rec" + str(max_rec))
+    print("noisefactor" + str(noisefactor))
+    
+    if max_rec < 0.2:
+        noisefactor_min = noisefactor
+        noisefactor = (noisefactor + noisefactor_max)/2
+        if abs(noisefactor - noisefactor_max) < 0.0001:
+            raise RuntimeError("didn't find an apropriate noisefactor, is the microfon plugged in, the dezibelmeter at the right attenuation and the amplifier turned on?")
+    elif max_rec > 0.25:
+        noisefactor_max = noisefactor
+        noisefactor = (noisefactor + noisefactor_min)/2
+        if abs(noisefactor - noisefactor_min) < 0.0001:
+            raise RuntimeError("didn't find an apropriate noisefactor, is the microfon plugged in, the dezibelmeter at the right attenuation and the amplifier turned on?")
+    else:
+        print("found an apropriate noisefactor")
+        break
 
 
-
-
-file = open("equalizer kleiner lautsprecher.npy", "rb")
-h_inv = np.load(file,allow_pickle = False,fix_imports = False)
-file.close()
 
 # Signals
-x = np.random.randn(fs*duration)/20#*np.hamming(fs*duration) # playback signal, duration seconds of white Gaussian noise
-print(max(x))
-#d = np.convolve(x,h)
-#trigger = np.ones(fs*duration)
-#silence = np.zeros(int(fs*0.5))
-#trigger = np.concatenate((silence,trigger))
-#zeroArray = np.zeros(fs*duration + int(0.5*fs))
-#matrixToPlay=np.dstack((np.concatenate((silence,x))/3,np.concatenate((silence,x))/3,trigger,trigger,trigger,trigger,trigger,trigger))[0]
-d = sd.playrec(np.convolve(x,h_inv), samplerate = fs, channels = 1,device = [1,7])
-time.sleep(duration+3)
+x = np.random.randn(fs*duration)*noisefactor#*np.hamming(fs*duration) # playback signal, duration seconds of white Gaussian noise
+
+to_play = np.dstack((np.zeros(len(x)),x))[0]
+
+d = sd.playrec(to_play, samplerate = fs, channels = 1,device = [1,7])
+time.sleep(duration+2)
+
 print(max(d))
-#sd.play(d, samplerate = fs,device = [1,7])
-#time.sleep(duration+3)
-#sd.play(d,samplerate = fs)
-#time.sleep(duration+2)
-d = d.transpose()[0]
+
 for i,item in enumerate(d):
     if item > 0.03:
         print(i)
-        d = d[i-60:]
+        d = d[i-80:]
         break
-#d , ok= perform_measurement(duration+3,fs,duration)
-#sd.play(d, samplerate = fs, device = 'Speakers (ASUS Essence STX II Audio)')#recorded Signal
 print(max(d))
-#d= np.convolve(d,h)
-#d = np.concatenate((np.zeros(512), d))
-#
 
-#sd.play(d,samplerate = 96000, device = 7)
+
 
 
 h_hat = np.zeros(L) # estimate of the acoustic impulse response from above, used in LMS algorithm, initally all zero
@@ -113,32 +118,16 @@ e_sys = np.zeros(len(x))# system distance, i.e., "how well" the impulse response
 
 # LMS algorithm to identify impulse response
 buffer_in = np.zeros(len(h_hat))
-for kSample in range(len(d)):
+for kSample in range(len(d)-2*L):
     
     buffer_in = np.concatenate((x[kSample:kSample+1], buffer_in[:-1]))
-    
+   # print(len(buffer_in))
+    #print(len(h_hat))
     y = np.dot(buffer_in.transpose() , h_hat) # filter input with h_hat, 
     e[kSample] = d[kSample] - y # compute error signal by difference of estimated and true signal
     
     # (N)LMS filter update
     h_hat = h_hat + mu * ((buffer_in * e[kSample]) / (sum(np.absolute(buffer_in)**2)+np.finfo(float).eps))
-    #was heißt da das eps??? 
-    
-    #e_sys[kSample] = sum(np.absolute(h_hat - h)**2)
-    
-#    if mod(kSample,1000) == 0 && 0
-#        figure(10);
-#        subplot(211);
-#        plot(10*log10(abs(e).^2));
-#        title('residual error signal power (logarithmic)');
-#        
-#        subplot(212);
-#        plot(10*log10(e_sys./e_sys(1)));
-#        title('normalized system distance (logarithmic)');
-#        
-#        pause(0.05);
-#    end
-
 
 # Performance measures for identification
 fig10 = plt.figure(10)
@@ -146,10 +135,11 @@ ax = fig10.add_subplot(211)
 ax.plot(10*np.log10(np.absolute(e)**2))
 plt.title('residual error signal power (logarithmic)')
 simpleaxis(ax)
-#plt.subplot(212)
-#plt.plot(10*np.log10(e_sys/e_sys[1]))
-#plt.title('normalized system distance (logarithmic)')
-#print(" ")
+file = open("residual error2.npy", "wb")
+np.save(file,e,allow_pickle = False,fix_imports = False)
+file.close()
+
+
 # Determine equalization filter
 H = np.fft.fft(h_hat, nFFT) # transform into frequency domain with additional zero-padding
 
@@ -158,7 +148,12 @@ H = np.fft.fft(h_hat, nFFT) # transform into frequency domain with additional ze
 H_inv = (np.exp(-1j*np.angle(H)))/(np.absolute(H)+delta)
 #entfernen der hohen frequenzen weil das mikro da nicht mehr passt
 #trotzdem Messung mit hoher sample rate, weil weit weg von nyq-freq
-H_inv[int(len(H_inv)/4):int(len(H_inv)*3/4)] = np.ones(int(len(H_inv)*3/4)-int(len(H_inv)/4))
+mean_12_bis_20_kHz = np.mean(H_inv[int(len(H_inv)/8):int(len(H_inv)*(20000/fs))])
+H_inv[int(len(H_inv)*(20000/fs)):int(len(H_inv)*((fs-20000)/fs))] = np.ones(int(len(H_inv)*((fs-20000)/fs))-int(len(H_inv)*(20000/fs)))*mean_12_bis_20_kHz
+
+mean_2_bis_4_kHz = np.mean(H_inv[int(len(H_inv)*(2000/fs)):int(len(H_inv)*(4000/fs))])
+H_inv[int(len(H_inv)*(0/fs)):int(len(H_inv)*((2000)/fs))] = np.ones(int(len(H_inv)*((2000)/fs))-int(len(H_inv)*(0/fs)))*mean_2_bis_4_kHz
+
 
 
 h_inv_raw = np.fft.ifft(H_inv, nFFT)
@@ -187,11 +182,8 @@ print(" ")
 simpleaxis(ax)
 
 
-
-
-
 # Visualization of equalized impulse response and magnitude frequency response
-fig2 = plt.figure(2)
+fig2 = plt.figure(2,figsize=(8,8))
 ax = fig2.add_subplot(211)
 plt.plot(h_hat)
 plt.xlabel('tap')
@@ -200,23 +192,20 @@ plt.title('time-domain impulse response')
 simpleaxis(ax)
 
 ax2 = fig2.add_subplot(212)
-#TODO hier 10 oder 20*log(*2)???
-ax2.plot(np.linspace(0,fs/2,num=int(len(h_hat)/2)), 10*np.log10(np.absolute(np.fft.fft(h_hat)[:int(len(h_hat)/2)])))
+
+ax2.plot(np.linspace(0,fs/4,num=int(len(h_hat)/4)), 10*np.log10(np.absolute(np.fft.fft(h_hat)[:int(len(h_hat)/4)])))
 plt.xlabel('frequency [hz]')
 plt.ylabel('magnitude [dB]')
 plt.title('magnitude response')
 simpleaxis(ax2)
 print(" ")
-### Comparison of magnitude responses
-#plt.figure(3)
-#plt.plot(np.linspace(0,fs/2,L), 10*np.log10(np.absolute(np.fft.fft(h))),'k','DisplayName','Unequalized')
-#plt.plot(np.linspace(0,fs/2,L+L_eq-1), 10*np.log10(np.absolute(np.fft.fft(h_eq))),'r','LineWidth',2,'DisplayName','Equalized')
-#plt.xlabel('frequency [Hz]')
-#plt.ylabel('magnitude [dB]')
-#plt.figure(3)
-#plt.subplot(211)
-#plt.plot(h_inv)
-#plt.xlabel('tap')
-#plt.ylabel('amplitude')
-#plt.title('inverse time-domain impulse response')
-#print(" ")
+
+file = open("h of system2.npy", "wb")
+np.save(file,h_hat,allow_pickle = False,fix_imports = False)
+file.close()
+
+
+file = open("equalizer schreckstimulus.npy", "wb")
+np.save(file,h_inv,allow_pickle = False,fix_imports = False)
+file.close()
+

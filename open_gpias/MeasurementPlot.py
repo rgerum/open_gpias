cd4bdc2389c807d621192364db2843a5f5eb97e3
Randrian <Randrian@localhost>
2018-11-19 17:29:32 +0100

    added copyright header to all .py files

--- open_gpias/MeasurementPlot.py
+++ open_gpias/MeasurementPlot.py
@@ -1,11 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# MeasurementPlot.py
 
-"""
-Created on Mon Dec  4 09:35:19 2017
-
-@author: strebms
-"""
+# Copyright (c) 2018, Richard Gerum, Achim Schilling, Hinrich Rahlfs, Matthias Streb
+#
+# This file is part of ASR-Setup.
+#
+# ASR-Setup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ASR-Setup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ASR-Setup. If not, see <http://www.gnu.org/licenses/>
 
 from qtpy import QtCore, QtGui, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
--- open_gpias/playlist/FrontendPlaylist.py
+++ open_gpias/playlist/FrontendPlaylist.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  9 10:12:35 2017
+# FrontendPlaylist.py
 
-@author: rahlfshh
-"""
+# Copyright (c) 2018, Richard Gerum, Achim Schilling, Hinrich Rahlfs, Matthias Streb
+#
+# This file is part of ASR-Setup.
+#
+# ASR-Setup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ASR-Setup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ASR-Setup. If not, see <http://www.gnu.org/licenses/>
+
 import sys
 from qtpy import QtCore, QtGui, QtWidgets
 import copy
